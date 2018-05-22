deb7cb783c27ee8b4180bbbb8bbad222991c1db3
swarup.mandal <swarup.mandal@SWARUPJAVA-PC.indusnettechnologies.com>
2018-05-22 17:25:24 +0530

    #

---
+++ TestProject/src/com/getssets/AccessMain.java
@@ -0,0 +1,13 @@
+package com.getssets;
+
+
+public class AccessMain {
+
+	public static void main(String[] args) {
+		
+		Address address = new Address(1, "kolkata");
+		
+		
+	}
+	
+}
---
+++ TestProject/src/com/getssets/Address.java
@@ -0,0 +1,14 @@
+package com.getssets;
+
+public class Address {
+	
+	private int id;
+	private String name;
+	
+	public Address(int id, String name) {
+		this.id = id;
+		this.name = name;
+		
+	}
+	
+}
