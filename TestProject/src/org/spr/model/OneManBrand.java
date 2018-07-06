e8c4c48483d49b4385b06077870216afbf99a37d
Swarup.mandal <Swarup.mandal@SWARUPJAVA-PC.indusnettechnologies.com>
2018-07-06 15:51:50 +0530

    #

--- TestProject/src/org/spr/model/OneManBrand.java
+++ TestProject/src/org/spr/model/OneManBrand.java
@@ -1,12 +1,41 @@
 package org.spr.model;
 
+import java.util.Collection;
+
 public class OneManBrand implements Performer {
 
+	private String name;
+	private Collection<Instrument> instruments;
+	
+	public OneManBrand() {
+		
+	}
+	
 	@Override
 	public void perform() {
 		
+		System.out.println(name + "Is playing");
+		
+		for(Instrument instrument : instruments){
+			System.out.println(instrument);
+		}
 		
 	}
 
+	public Collection<Instrument> getInstruments() {
+		return instruments;
+	}
+
+	public void setInstruments(Collection<Instrument> instruments) {
+		this.instruments = instruments;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
